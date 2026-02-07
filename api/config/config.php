f4df62e4c7eb185beca6aecb505de9dd1fa25936
mahmoudharasis <55611757+mahmoudharasis@users.noreply.github.com>
2026-02-07 15:41:35 +0300

    Create application configuration constants in config.php

---
+++ api/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('APP_ENV', 'production');
+define('APP_DEBUG', false);
+define('APP_URL', 'https://yourapp.com');
+define('DB_HOST', 'localhost');
+define('DB_DATABASE', 'your_database');
+define('DB_USERNAME', 'your_username');
+define('DB_PASSWORD', 'your_password');
+
+?>
