5a9fc0dbb1ad50581328103be4d49718eb5b88fe
mahmoudharasis <55611757+mahmoudharasis@users.noreply.github.com>
2026-02-07 15:42:11 +0300

    Create api/folders.php with folder management API endpoints

---
+++ api/folders.php
@@ -0,0 +1,30 @@
+<?php
+
+// Folder management API endpoints
+
+// Create a folder
+function createFolder($name) {
+    // Your code here to create a folder
+}
+
+// List folders
+function listFolders() {
+    // Your code here to list folders
+}
+
+// Rename a folder
+function renameFolder($oldName, $newName) {
+    // Your code here to rename a folder
+}
+
+// Delete a folder
+function deleteFolder($name) {
+    // Your code here to delete a folder
+}
+
+// Move a folder
+function moveFolder($name, $newLocation) {
+    // Your code here to move a folder
+}
+
+?>
